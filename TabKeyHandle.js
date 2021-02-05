36b02837ad7615bd90bcaeadaf0dd194a98d1c0e
michal.bernat <michal.bernat@oxyshop.cz>
2021-02-05 12:05:51 +0100

    [UTIL-1] Removed ng- prefix

--- TabKeyHandle.js
+++ TabKeyHandle.js
@@ -4,21 +4,21 @@ export default class TabKeyHandle {
   }
 
   init() {
-    const focusableWrapper = this.element.querySelectorAll('.ng-focusable-wrapper')
+    const focusableWrapper = this.element.querySelectorAll('.focusable-wrapper')
 
     focusableWrapper.forEach((item) => {
       item.addEventListener('keydown', (event) => {
-        const focusableChildren = item.querySelectorAll('.ng-first-focusable, .ng-last-focusable')
+        const focusableChildren = item.querySelectorAll('.first-focusable, .last-focusable')
         const srcElementClass = event.srcElement.classList
 
         if (event.keyCode === 9) {
-          if (event.shiftKey && srcElementClass.contains('ng-first-focusable')) {
+          if (event.shiftKey && srcElementClass.contains('first-focusable')) {
             focusableChildren.item(focusableChildren.length - 1).focus()
             event.preventDefault()
 
             return
           }
-          if (!event.shiftKey && srcElementClass.contains('ng-last-focusable')) {
+          if (!event.shiftKey && srcElementClass.contains('last-focusable')) {
             focusableChildren.item(0).focus()
             event.preventDefault()
           }
